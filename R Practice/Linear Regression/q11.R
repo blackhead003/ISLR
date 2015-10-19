94c1e996c6b68667a3a744a6c2cb6e9c8b6c3ec2
Rahul <blackhead003@gmail.com>
2015-10-19 10:36:14 +0530

    Added a few lines

--- R Practice/Linear Regression/q11.R
+++ R Practice/Linear Regression/q11.R
@@ -14,3 +14,11 @@ summary(fit2)
 
 # The basic relationship between both of them is that both are same. And y=2x+ε could also be written x=0.5(y−ε).
 
+t <- sqrt(length(x) - 1) * (x %*% y) / sqrt(sum(x^2) * sum(y^2) - (x %*% y)^2)
+as.numeric(t)
+
+#Regression with an intercept for y onto x and x onto y
+fit3 <- lm(y ~ x)
+fit4 <- lm(x ~ y)
+summary(fit3)
+summary(fit4)
